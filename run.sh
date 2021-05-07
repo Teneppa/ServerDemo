709b6a69f4f8ce8d3cb5c2c684b7023c169442dd
Teemu Laurila <laurilateemu8@gmail.com>
2021-05-07 09:22:49 +0300

    Initial commitment

---
+++ run.sh
@@ -0,0 +1,4 @@
+export FLASK_ENV=development
+export FLASK_APP=runFlaskSite
+
+flask run --host="0.0.0.0" --port=80
---
+++ runFlaskSite.py
@@ -0,0 +1,105 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/raw')
+def raw():
+    tempString = ""
+
+    with open('../NODEMCU/temp.txt') as f:
+        tempString = f.read()
+
+    return tempString
+
+@app.route('/')
+def hello():
+
+    tempString = ""
+    with open('../NODEMCU/temp.txt') as f:
+        tempString = f.read()
+
+    with open('iplist.txt', 'a') as f:
+        ip = request.remote_addr
+
+        if ip.split('.')[0] != "10":
+            print("IP: "+ip)
+            f.write(ip+"\n")
+        else:
+            print("LOCAL IP -> NOT ADDED")
+
+    site = """
+    <!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>
+<body>
+
+<h2>Test</h2>
+
+<table>
+  <tr>
+    <td>Thing</td>
+    <td>Temperature</td>
+    <td>Last updated: </td>
+  </tr>
+  <tr>
+    <td>Sensor1</td>
+    <td id="demo"></td>
+    <td id="update">dunno</td>
+  </tr>
+</table>
+
+
+<script>
+
+    // Load the sensor values when the page is loaded
+    document.addEventListener("DOMContentLoaded", function(){
+        loadDoc();
+    });
+
+    // Update the values every 5s
+    setInterval(loadDoc, 5000);
+
+    function loadDoc() {
+        var xhttp = new XMLHttpRequest();
+        
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("demo").innerHTML =
+                this.responseText;
+            }
+        };
+        
+        xhttp.open("GET", "raw", true);
+        xhttp.send();
+        
+        var dt = new Date();
+        document.getElementById("update").innerHTML = dt.getHours()+"."+dt.getMinutes()+"."+dt.getSeconds()+":"+dt.getMilliseconds();
+    }
+</script>
+
+</body>
+
+</html>
+
+    """
+
+    return site
